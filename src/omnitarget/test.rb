4bf8bedcbb1c73a3edd9819485cafce9ff9fb697
akshah123 <akshah123@gmail.com>
2013-01-14 18:06:41 -0500

    a

--- src/omnitarget/test.rb
+++ src/omnitarget/test.rb
@@ -21,55 +21,14 @@ end
 
 def get_random_click()
   transaction_id  =  get_random_string(50,false)
-  related_click = ""
-  is_unique = [true,false].sample
   offer_id = rand(1..1500)
-  aff_id = rand(1..1500)
-  aff_id_new_old = rand(1..1500)
-  aff_id_new = rand(1..1500)
-  url_id = rand(1..1500)
-  finance_rule_id = rand(1..2000)
-  ad_id = rand(1..1500)
-  campaign_id = rand(1..2000)
-  creative_id = rand(1..1500)
-  placement_id = rand(1..1500)
-  dma = rand(1..1500)
-  city = get_random_string(30)
-  state = get_random_string(15)
-  zip = get_random_string(10)
-  country = get_random_string(15)
-  latitude = rand(90)
-  longitude = rand(90)
-  image = get_random_string(10)
-  text  = get_random_string(10)
-  dynamic_location_text = get_random_string(10)
-  callout = get_random_string(10)
-  callout_text  = get_random_string(10)
-  animation  = get_random_string(10)
-  time_of_day= rand(0..23)
-  source  = get_random_string(15)
-  sub1  = get_random_string(15)
-  sub2 = get_random_string(15)
-  sub3  = get_random_string(15)
-  sub4  = get_random_string(15)
-  sub5  = get_random_string(15)
-  params  = get_random_string(15)
-  impression_cost  = rand(2)
-  cost = rand(50)
-  revenue = rand(100)
-  referrer = get_random_string(255)
-  browser  = get_random_string(250)
-  os  = get_random_string(15)
-  ip = "192.168.31.138"
-  is_valid = [true,false].sample
-  is_dynamic_revenue = [true,false].sample
-  return [transaction_id, related_click, is_unique, offer_id, aff_id, aff_id_new_old, aff_id_new, url_id, finance_rule_id, ad_id, campaign_id, creative_id, placement_id, dma, city, state, zip, country, latitude, longitude, image, text, dynamic_location_text, callout, callout_text, animation, time_of_day, source, sub1, sub2, sub3, sub4, sub5, params, impression_cost, cost, revenue, referrer, browser, os, ip]
+  return [transaction_id, offer_id]
 end
 
 def insert_click(client, click)
 	puts click
 	begin
-		client.call_procedure("TEST.insert", *click)
+		client.call_procedure("TEST.insert", click[0], click[1])
 	rescue VoltRb::VoltError => bang
 		puts "Error: #{bang.status_string}"
 	rescue Exception => e
