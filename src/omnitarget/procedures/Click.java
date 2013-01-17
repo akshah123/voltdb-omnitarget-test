1c02d847f8f592117883212fbef5f5a2b04a6a0d
akshah123 <akshah123@gmail.com>
2013-01-17 16:07:09 +0000

    Fixed stored procedure click.java

--- src/omnitarget/procedures/Click.java
+++ src/omnitarget/procedures/Click.java
@@ -37,6 +37,7 @@ import org.voltdb.VoltTable;
 
 import java.util.Date;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 @ProcInfo (
     partitionInfo = "click.offer_id:0",
@@ -57,7 +58,11 @@ public class Click extends VoltProcedure {
 		, float cost, float revenue, String referrer, String browser, String os, String ip) {
 
 		Date click_date = getTransactionTime();
-		Date click_date_interval = Calendar.set(click_date.getYear(), click_date.getMonth(), click_date.getHourOfDay(), 0);
+		Calendar calendar = GregorianCalendar.getInstance();
+		calendar.setTime(click_date);
+		Calendar intervalCalendar = GregorianCalendar.getInstance();
+		intervalCalendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.HOUR_OF_DAY),0);
+		Date click_date_interval = intervalCalendar.getTime();
 		
         // Post the vote
         voltQueueSQL(insertClickStmt, EXPECT_SCALAR_MATCH(1), transaction_id, click_date, click_date_interval, is_unique, offer_id, aff_id, url_id, finance_rule_id, ad_id, campaign_id, creative_id, placement_id, dma, city, state, zip, country, latitude, longitude, image, text, dynamic_location_text, source, sub1, sub2, sub3, sub4, sub5, cost, revenue, referrer, browser, os, ip);
